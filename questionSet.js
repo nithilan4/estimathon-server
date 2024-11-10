e4b2cf9ddcf1f2439d338308bdbad0df0525925c
Saksham Dhuria <107601836+sakshamdhuria@users.noreply.github.com>
2024-11-09 21:52:31 -0600

    Create questionSet.js

---
+++ questionSet.js
@@ -0,0 +1,82 @@
+let questionSet = [
+    {
+        id: 0,
+        q: "How many total permutations of the word MadHacks are there (consider no differences between lower and upper case characters)?",
+        a: 20160
+    },
+    {
+        id: 1,
+        q: "How many fewer passengers on US Airlines flew in 2020 compared to 2019 (in the millions)?",
+        a: 557
+    },
+    {
+        id: 2,
+        q: "How many roller coaster deaths have happened in the last decade?",
+        a: 40
+    },
+    {
+        id: 3,
+        q: "How much money has been bet on election on RobinHood on 4:20 PM CDT on November 5th 2024 (in the millions)?",
+        a: 283
+    },
+    {
+        id: 4,
+        q: "How many digits are there in 2024!?",
+        a: 283
+    },
+    {
+        id: 5,
+        q: "Operating Systems (CS 537) is one of the most difficult CS courses at UW-Madison, how many students have gotten an A in it according to MadGrades (data collected since Fall 2006)?",
+        a: 5815
+    },
+    {
+        id: 6,
+        q: "How many moons are there in the entire solar system?",
+        a: 2613
+    },
+    {
+        id: 7,
+        q: "How many (in million) followers does Elon Musk have on X?",
+        a: 293
+    },
+    {
+        id: 8,
+        q: "What is Giannis Antetokounmpo’s career NBA points scored?",
+        a: 203
+    },
+    {
+        id: 9,
+        q: "How many people have walked on the moon?",
+        a: 18688
+    },
+    {
+        id: 10,
+        q: "How many acres is the UW Madison Campus?",
+        a: 12
+    },
+    {
+        id: 11,
+        q: "How many pieces of art are displayed in the Louvre?",
+        a: 939
+    },
+    {
+        id: 12,
+        q: "How many times larger (in volume) is lake Michigan than lake Mendota?",
+        a: 35000
+    },
+    {
+        id: 13,
+        q: "What is the most amount of days Lake Mendota was frozen for in an annual year?",
+        a: 10225
+    },
+    {
+        id: 14,
+        q: "How many times taller is the Wisconsin State Capitol building than the average height of a Badger?",
+        a: 161
+    },
+    {
+        id: 15,
+        q: "How many people are playing League of Legends at the current moment?",
+        a: 284
+    }
+];
